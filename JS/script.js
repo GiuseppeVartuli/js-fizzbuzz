d6286d6ac38f32407cb00c6ee995919e4374d5c4
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2023-12-08 13:35:16 +0100

    feat text and color to box

--- JS/script.js
+++ JS/script.js
@@ -20,14 +20,28 @@ BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
 
+const containerElement = document.querySelector(".container");
+
 for (let i = 1; i <= 100; i++) {
+  const boxElement = document.createElement("div");
+  boxElement.classList.add("box");
+
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FrizzBuzz");
+    boxElement.innerText = "FrizzBuzz";
+    boxElement.classList.add("box_red");
   } else if (i % 5 === 0) {
     console.log("Buzz");
+    boxElement.innerText = "Buzz";
+    boxElement.classList.add("box_blue");
   } else if (i % 3 === 0) {
     console.log("Frizz");
+    boxElement.innerText = "Frizz";
+    boxElement.classList.add("box_yellow");
   } else {
     console.log(i);
+    boxElement.innerText = i;
+    boxElement.classList.add("box_green");
   }
+  containerElement.append(boxElement);
 }
